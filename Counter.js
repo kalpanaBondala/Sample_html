3ae00c995177b6ce439a472564d01e250f118832
kalpanaBondala <71065151+kalpanaBondala@users.noreply.github.com>
2020-09-25 19:55:06 +0530

    Counter_inc_dec.js

---
+++ Counter.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class Counter extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            counterval :0
+        }
+
+    }
+
+    onIncreamentClick(value=0) {
+        //console.log(this.state.counterval);
+        value= value + 1;
+        this.setState({counterval:value})
+        console.log(value);
+
+        //alert("click on me to increament" +value);
+
+    }
+
+
+    onDecreamentClick(value=0) {
+        value= value - 1;
+        this.setState({counterval:value})
+        console.log(value);
+
+    }
+    render() {
+        return (<React.Fragment>
+                <p> Counter value is {this.state.counterval} </p>
+                <input type="button" value="+" onClick={(value)=>this.onIncreamentClick(this.state.counterval)}/>
+                <br></br>
+                <input type="button" value="-" onClick={(value)=>this.onDecreamentClick(this.state.counterval)}/>
+            </React.Fragment>)
+        }    
+    }
+
+    export default Counter;
+
+
+
+
