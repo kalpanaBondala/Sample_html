e330e3978d7ffe1780eec921817f2b8e1c76d6fe
kalpanaBondala <71065151+kalpanaBondala@users.noreply.github.com>
2020-10-21 17:34:04 +0530

    Add files via upload

---
+++ Routing/AboutUs.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+
+export class AboutUs extends Component {
+    render() {
+      return (
+          <div>
+            <h2>AboutUs !!!</h2>
+          </div>
+      );
+    }
+  }
+   
+
+
+export default AboutUs;
